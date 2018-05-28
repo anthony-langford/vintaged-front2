d7ec089bee679e1baf8296c5ab100915f5b25f59
Anthony Langford <anthony@evree.ca>
2018-05-28 15:38:49 -0400

    move actions into folder and create index file

---
+++ src/actions/common.js
@@ -0,0 +1,23 @@
+// common actions
+const commonActions = {
+  testAction: bool => ({
+    type: 'testAction',
+    bool,
+  })
+};
+
+export default commonActions;
+
+// import {CHANGE, CHANGE_BACK, FETCH_VINTAGES} from '../actionTypes';
+
+// export const change = function change() {
+//   return {type: CHANGE}
+// };
+
+// export const changeBack = function changeBack() {
+//   return {type: CHANGE_BACK}
+// };
+
+// export const fetchVintages = function fetchVintages(list) {
+//   return {type: FETCH_VINTAGES, list: list}
+// };
