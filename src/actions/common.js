56831fca5a997c09fdc0b7d4766ee716772e65fc
Anthony Langford <anthony@evree.ca>
2018-05-28 18:15:22 -0400

    add wine fetching actions

--- src/actions/common.js
+++ src/actions/common.js
@@ -1,23 +1,52 @@
-// common actions
+import 'whatwg-fetch';
+import API from '../helpers/API';
+
+// Common actions
 const commonActions = {
   testAction: bool => ({
     type: 'testAction',
     bool,
+  }),
+
+  requestWines: () => ({
+    type: 'REQUEST_WINES',
+  }),
+
+  receiveWines: wines => ({
+    type: 'RECEIVE_WINES',
+    wines,
+    receivedAt: Date.now(),
+  }),
+
+  fetchWines: () => (
+    dispatch => {
+      dispatch(commonActions.requestWines());
+      return fetch(`${API.WINES}`, {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        }),
+      })
+        .then((response) => {
+          // Return an empty object if there are no wines
+          if (response.status === 404) {
+            return {};
+          }
+
+          return response.json();
+        })
+        .then(json => {
+          console.log('json response', json);
+          dispatch(commonActions.receiveWines(json));
+        })
+        .catch(error => console.log(error));
+    }
+  ),
+
+  clearStore: () => ({
+    type: 'CLEAR_STORE',
   })
 };
 
 export default commonActions;
-
-// import {CHANGE, CHANGE_BACK, FETCH_VINTAGES} from '../actionTypes';
-
-// export const change = function change() {
-//   return {type: CHANGE}
-// };
-
-// export const changeBack = function changeBack() {
-//   return {type: CHANGE_BACK}
-// };
-
-// export const fetchVintages = function fetchVintages(list) {
-//   return {type: FETCH_VINTAGES, list: list}
-// };
