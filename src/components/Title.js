02e77749472442794136b098dfc1edd6e8070a73
Anthony Langford <anthony@evree.ca>
2018-05-30 16:07:30 -0400

    👾

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import styled from 'react-emotion';
+
+const Title = styled('h1')`
+font-family: 'Arial, Helvetica, sans-serif';
+text-align: center;
+font-size: 1.6em;
+padding: 10px;
+margin: 0;
+color: #fffa0e;
+`;
+
+export default Title;
