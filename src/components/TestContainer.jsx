b26d265bff5d4b7f675a1cc88ef18981b84643c6
Anthony Langford <anthony.langford@gmail.com>
2018-06-13 14:52:24 -0400

    💥

--- src/components/TestContainer.jsx
+++ src/components/TestContainer.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Provider, connect} from 'react-redux';
-import {createStore, combineReducers} from 'redux';
-import { ThemeProvider } from 'emotion-theming';
-import '../styles/globalStyles';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { ThemeProvider } from 'emotion-theming'
 
 // Import helpers
-import store from '../store';
-import {commonActions} from '../actions';
+import store from '../store'
+import {commonActions} from '../actions'
 
 // Import components
-import Title from '../components/Title';
-import Background from '../components/Background';
-import CelestialObject from '../components/CelestialObject';
+import Title from '../components/Title'
+import Background from '../components/Background'
+import CelestialObject from '../components/CelestialObject'
 
 
 
@@ -21,55 +19,55 @@ const dayTheme = {
   skyColor: '#37d8e6',
   celestialObjectColor: '#ffdd00',
   celestialObjectBorderColor: '#f1c40f'
-};
+}
 
 const nightTheme = {
   skyColor: '#2c3e50',
   celestialObjectColor: '#bdc3c7',
   celestialObjectBorderColor: '#eaeff2'
-};
+}
 
 
 
 class TestContainer extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     // Set initial state for the container
     this.state = {
       title: 'Vintaged',
       isDay: true,
       theme: dayTheme,
-    };
+    }
   }
 
   componentWillMount() {   
     // Display loader before mount
-    // store.dispatch(commonActions.toggleLoader(true));
+    // store.dispatch(commonActions.toggleLoader(true))
 
-    store.dispatch(commonActions.fetchWines());
+    store.dispatch(commonActions.fetchWines())
 
     // Bind functions to global scope
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     // Hide loader on mount
-    // store.dispatch(commonActions.toggleLoader(false));
+    // store.dispatch(commonActions.toggleLoader(false))
   }
 
   componentWillUnmount() {
     // Display loader on unmount
-    // store.dispatch(commonActions.toggleLoader(true));
+    // store.dispatch(commonActions.toggleLoader(true))
   }
 
   handleClick() {
-    const isDay = this.state.isDay;
+    const isDay = this.state.isDay
     this.setState({
       isDay: !isDay,
       theme: isDay ? nightTheme : dayTheme,
       title: isDay ? 'fuck yeah 🍾' : 'Vintaged',
-    });
+    })
   }
 
   render() {
@@ -83,25 +81,25 @@ class TestContainer extends React.Component {
           </CelestialObject>
         </Background>
       </ThemeProvider>
-    );
+    )
   }
 }
 
 function mapStateToProps(state) {
   return {
     ...state,
-  };
+  }
 }
 
 // Static type checking for props
 TestContainer.propTypes = {
   // optionalPropThatIsAnObject: PropTypes.objectOf(PropTypes.shape),
   dispatch: PropTypes.func.isRequired,
-};
+}
 
 // Set default value for prop if not required and not present
 TestContainer.defaultProps = {
   // optionalPropThatIsAnObject: {},
-};
+}
 
-export default connect(mapStateToProps)(TestContainer);
+export default connect(mapStateToProps)(TestContainer)
--- src/styles/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import { injectGlobal } from 'react-emotion';
-
-injectGlobal`
-  html, body, #root {
-    height: 100%;
-    padding: 0;
-    margin: 0; 
-  }
-`
